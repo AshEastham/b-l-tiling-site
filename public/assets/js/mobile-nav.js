660d3737752b2e67ee20bb3b901683a06365a836
Ash Eastham <ash-eastham@hotmail.co.uk>
2021-02-03 14:46:09 +0000

    Edited navigation for mobile view, also added media queries for responsiveness

---
+++ public/assets/js/mobile-nav.js
@@ -0,0 +1,17 @@
+/* Open */
+function openNav() {
+    document.getElementById("nav").style.display = "flex";
+    document.getElementById("nav").style.height = "100%";
+    document.getElementById("nav").style.width = "100%";
+    document.getElementById("open-btn").style.display = "none";
+    document.getElementById("close-btn").style.display = "flex";
+}
+  
+/* Close */
+function closeNav() {
+    document.getElementById("nav").style.height = "0%";
+    document.getElementById("nav").style.width = "100%";
+    document.getElementById("nav").style.display = "none";
+    document.getElementById("open-btn").style.display = "flex";
+    document.getElementById("close-btn").style.display = "none";
+}
